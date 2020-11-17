4b5adbf130701445f05b7cafc8f9227c59198b5d
Sladrigezz <Sladrigezz@github.com>
2020-11-17 19:31:30 +0300

    now playing

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import { fetchMovies } from "../../service";
+import RBCarousel from "react-bootstrap-carousel";
+import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
+
+export function Home() {
+  const [nowPlaying, setNewPlaying] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setNewPlaying(await fetchMovies());
+    };
+    fetchAPI();
+  }, []);
+
+  const movies = nowPlaying.slice(0, 5).map((item, index) => {
+    return (
+      <div style={{ height: 500, width: "100%" }} key={index}>
+        <div className="carousel-center">
+          <img style={{ height: 600 }} src={item.backPoster} alt={item.title} />
+        </div>
+        <div className="carousel-center">
+          <i
+            className="far fa-play-circle"
+            style={{ fontSize: 95, color: "#f4c10f" }}
+          ></i>
+        </div>
+        <div
+          className="carousel-caption"
+          style={{ textAlign: "center", fontSize: 35 }}
+        >
+          {item.title}
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className="container">
+      <div className="row mt-2">
+        <div className="col">
+          <RBCarousel
+            autoplay={true}
+            pauseOnVisibility={true}
+            slidesshowSpeed={5000}
+            version={4}
+            indicators={false}
+          >
+            {movies}
+          </RBCarousel>
+        </div>
+      </div>
+    </div>
+  );
+}
