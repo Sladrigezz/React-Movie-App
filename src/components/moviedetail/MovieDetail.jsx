79bc22bde65813ba496dfe1c6d020d536339c9cc
Sladrigezz <Sladrigezz@github.com>
2020-11-18 12:50:53 +0300

    details

--- src/components/moviedetail/MovieDetail.jsx
+++ src/components/moviedetail/MovieDetail.jsx
@@ -1,9 +1,15 @@
-import React, { useEffect, useState } from "react";
-import { fetchMovieDetail, fetchMovieVideos } from "../../service";
+import React, { useState, useEffect } from "react";
+import {
+  fetchMovieDetail,
+  fetchMovieVideos,
+  fetchCasts,
+  fetchSimilarMovie,
+} from "../../service";
 import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
 import { Modal } from "react-bootstrap";
 import ReactPlayer from "react-player";
 import ReactStars from "react-rating-stars-component";
+import { Link } from "react-router-dom";
 
 export function MovieDetail({ match }) {
   let params = match.params;
@@ -11,11 +17,15 @@ export function MovieDetail({ match }) {
   const [isOpen, setIsOpen] = useState(false);
   const [detail, setDetail] = useState([]);
   const [video, setVideo] = useState([]);
+  const [casts, setCasts] = useState([]);
+  const [similarMovie, setSimilarMovie] = useState([]);
 
   useEffect(() => {
     const fetchAPI = async () => {
       setDetail(await fetchMovieDetail(params.id));
       setVideo(await fetchMovieVideos(params.id));
+      setCasts(await fetchCasts(params.id));
+      setSimilarMovie(await fetchSimilarMovie(params.id));
     };
 
     fetchAPI();
@@ -23,8 +33,8 @@ export function MovieDetail({ match }) {
 
   genres = detail.genres;
 
-  const MoviePlayerModal = (props) => {
-    const youtubeUrl = `https://www.youtube.com/watch?v=`;
+  const MoviePalyerModal = (props) => {
+    const youtubeUrl = "https://www.youtube.com/watch?v=";
     return (
       <Modal
         {...props}
@@ -65,21 +75,61 @@ export function MovieDetail({ match }) {
     });
   }
 
+  const castList = casts.slice(0, 4).map((c, i) => {
+    return (
+      <div className="col-md-3 text-center" key={i}>
+        <img
+          className="img-fluid rounded-circle mx-auto d-block"
+          src={c.img}
+          alt={c.name}
+        ></img>
+        <p className="font-weight-bold text-center">{c.name}</p>
+        <p
+          className="font-weight-light text-center"
+          style={{ color: "#5a606b" }}
+        >
+          {c.character}
+        </p>
+      </div>
+    );
+  });
+
+  const similarMovieList = similarMovie.slice(0, 4).map((item, index) => {
+    return (
+      <div className="col-md-3 col-sm-6" key={index}>
+        <div className="card">
+          <Link to={`/movie/${item.id}`}>
+            <img className="img-fluid" src={item.poster} alt={item.title}></img>
+          </Link>
+        </div>
+        <div className="mt-3">
+          <p style={{ fontWeight: "bolder" }}>{item.title}</p>
+          <p>Rated: {item.rating}</p>
+          <ReactStars
+            count={item.rating}
+            size={20}
+            color1={"#f4c10f"}
+          ></ReactStars>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div className="container">
       <div className="row mt-2">
-        <MoviePlayerModal
+        <MoviePalyerModal
           show={isOpen}
           onHide={() => {
             setIsOpen(false);
           }}
-        ></MoviePlayerModal>
+        ></MoviePalyerModal>
         <div className="col text-center" style={{ width: "100%" }}>
           <img
-            src={`https://image.tmdb.org/t/p/original/${detail.backdrop_path}`}
-            alt={detail.title}
             className="img-fluid"
-          />
+            src={`http://image.tmdb.org/t/p/original/${detail.backdrop_path}`}
+            alt={detail.title}
+          ></img>
           <div className="carousel-center">
             <i
               onClick={() => setIsOpen(true)}
@@ -95,16 +145,19 @@ export function MovieDetail({ match }) {
           </div>
         </div>
       </div>
+
       <div className="row mt-3">
         <div className="col">
           <p style={{ color: "#5a606b", fontWeight: "bolder" }}>GENRE</p>
         </div>
       </div>
+
       <div className="row mt-3">
         <div className="col">
           <ul className="list-inline">{genresList}</ul>
         </div>
       </div>
+
       <div className="row mt-3">
         <div className="col">
           <div className="text-center">
@@ -114,6 +167,118 @@ export function MovieDetail({ match }) {
               color1={"#f4c10f"}
             ></ReactStars>
           </div>
+          <div className="mt-3">
+            <p style={{ color: "#5a606b", fontWeight: "bolder" }}>OVERVIEW</p>
+            {detail.overview}
+          </div>
+        </div>
+      </div>
+
+      <div className="row mt-3">
+        <div className="col-md-3">
+          <p style={{ color: "#5a606b", fontWeight: "bolder" }}>RELEASE DATE</p>
+          <p style={{ color: "#f4c10f" }}>{detail.release_date}</p>
+        </div>
+        <div className="col-md-3">
+          <p style={{ color: "#5a606b", fontWeight: "bolder" }}>RUN TIME</p>
+          <p style={{ color: "#f4c10f" }}>{detail.runtime}</p>
+        </div>
+        <div className="col-md-3">
+          <p style={{ color: "#5a606b", fontWeight: "bolder" }}>BUDGET</p>
+          <p style={{ color: "#f4c10f" }}>{detail.budget}</p>
+        </div>
+        <div className="col-md-3">
+          <p style={{ color: "#5a606b", fontWeight: "bolder" }}>HOMEPAGE</p>
+          <p style={{ color: "#f4c10f" }}>{detail.homepage}</p>
+        </div>
+      </div>
+
+      <div className="row mt-3">
+        <div className="col">
+          <p style={{ color: "#5a606b", fontWeight: "bolder" }}>CASTS</p>
+        </div>
+      </div>
+      <div className="row mt-3">{castList}</div>
+
+      <div className="row mt-3">
+        <div className="col">
+          <p style={{ color: "#5a606b", fontWeight: "bolder" }}>
+            SIMILAR MOVIES
+          </p>
+        </div>
+      </div>
+
+      <div className="row mt-3">{similarMovieList}</div>
+
+      <hr className="mt-5" style={{ borderTop: "1px solid #5a606b" }}></hr>
+
+      <div className="row mt-3">
+        <div className="col-md-8 col-sm-6" style={{ color: "#5a606b" }}>
+          <h3>ABOUT PROJECT</h3>
+          <p>CREATED USING REACT</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti
+            laborum nihil, laudantium laboriosam dolorem optio assumenda
+            recusandae corporis voluptatum dolorum pariatur ullam quam minima
+            harum fuga id tempora exercitationem repellat.
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Repellendus doloremque, sequi exercitationem, vel ex voluptatibus,
+            veritatis esse officiis architecto et iure earum mollitia pariatur
+            recusandae voluptates saepe porro nihil quas?
+          </p>
+          <ul className="list-inline">
+            <li className="list-inline-item">
+              <a href="/" style={{ color: "#f4c10f" }}>
+                <i className="fab fa-facebook"></i>
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a href="/" style={{ color: "#f4c10f" }}>
+                <i className="fab fa-vk"></i>
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a href="/" style={{ color: "#f4c10f" }}>
+                <i className="fab fa-instagram"></i>
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a href="/" style={{ color: "#f4c10f" }}>
+                <i className="fab fa-youtube"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-md-4 col-sm-6" style={{ color: "#5a606b" }}>
+          <h3>KEEP IN TOUCH</h3>
+          <ul className="list-unstyled">
+            <li>
+              <p>
+                <strong>
+                  <i className="fas fa-map-marker-alt"></i> Addres:
+                </strong>{" "}
+                Moscow, Russia
+              </p>
+            </li>
+            <li>
+              <p>
+                <strong>
+                  <i className="fas fas fa-phone"></i> Phone
+                </strong>{" "}
+                +8 (123) 456 78 90
+              </p>
+            </li>
+            <li>
+              <p>
+                <strong>
+                  <i className="fas fa-envelope"></i> Email:
+                </strong>{" "}
+                Email@email.ru
+              </p>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
--- src/service/index.js
+++ src/service/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiKey = "fe476699fbac239102ffd3a2f065c098";
+const apiKey = "a4999a28333d1147dbac0d104526337a";
 const url = "https://api.themoviedb.org/3";
 const nowPlayingUrl = `${url}/movie/now_playing`;
 const topratedUrl = `${url}/movie/top_rated`;
@@ -141,6 +141,43 @@ export const fetchMovieVideos = async (id) => {
   } catch (error) {}
 };
 
-export const fetchCasts = () => {};
+export const fetchCasts = async (id) => {
+  try {
+    const { data } = await axios.get(`${movieUrl}/${id}/credits`, {
+      params: {
+        api_key: apiKey,
+      },
+    });
+    const modifiedData = data["cast"].map((c) => ({
+      id: c["cast_id"],
+      character: c["character"],
+      name: c["name"],
+      img: "https://image.tmdb.org/t/p/w200" + c["profile_path"],
+    }));
 
-export const fetchSimilarMovie = () => {};
+    return modifiedData;
+  } catch (error) {}
+};
+
+export const fetchSimilarMovie = async (id) => {
+  try {
+    const { data } = await axios.get(`${movieUrl}/${id}/similar`, {
+      params: {
+        api_key: apiKey,
+        language: "en_US",
+      },
+    });
+    const posterUrl = "https://image.tmdb.org/t/p/original/";
+    const modifiedData = data["results"].map((m) => ({
+      id: m["id"],
+      backPoster: posterUrl + m["backdrop_path"],
+      popularity: m["popularith"],
+      title: m["title"],
+      poster: posterUrl + m["poster_path"],
+      overview: m["overview"],
+      rating: m["vote_average"],
+    }));
+
+    return modifiedData;
+  } catch (error) {}
+};
