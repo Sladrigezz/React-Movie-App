d7a442eb5c6476af7e2e3277345dfe865104bfa2
Sladrigezz <Sladrigezz@github.com>
2020-11-18 12:10:14 +0300

    trailer, genres

--- src/components/moviedetail/MovieDetail.jsx
+++ src/components/moviedetail/MovieDetail.jsx
@@ -1,9 +1,121 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchMovieDetail, fetchMovieVideos } from "../../service";
+import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
+import { Modal } from "react-bootstrap";
+import ReactPlayer from "react-player";
+import ReactStars from "react-rating-stars-component";
 
-export function MovieDetail() {
+export function MovieDetail({ match }) {
+  let params = match.params;
+  let genres = [];
+  const [isOpen, setIsOpen] = useState(false);
+  const [detail, setDetail] = useState([]);
+  const [video, setVideo] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setDetail(await fetchMovieDetail(params.id));
+      setVideo(await fetchMovieVideos(params.id));
+    };
+
+    fetchAPI();
+  }, [params.id]);
+
+  genres = detail.genres;
+
+  const MoviePlayerModal = (props) => {
+    const youtubeUrl = `https://www.youtube.com/watch?v=`;
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title
+            id="contained-modal-title-vcenter"
+            style={{ color: "#000000", fontWeight: "bolder" }}
+          >
+            {detail.title}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body style={{ backgroundColor: "#000000" }}>
+          <ReactPlayer
+            className="container-fluid"
+            url={youtubeUrl + video.key}
+            playing
+            width="100%"
+          ></ReactPlayer>
+        </Modal.Body>
+      </Modal>
+    );
+  };
+
+  let genresList;
+  if (genres) {
+    genresList = genres.map((g, i) => {
+      return (
+        <li className="list-inline-item" key={i}>
+          <button type="button" className="btn btn-outline-info">
+            {g.name}
+          </button>
+        </li>
+      );
+    });
+  }
+
   return (
-    <div>
-      <h1>MovieDetail</h1>
+    <div className="container">
+      <div className="row mt-2">
+        <MoviePlayerModal
+          show={isOpen}
+          onHide={() => {
+            setIsOpen(false);
+          }}
+        ></MoviePlayerModal>
+        <div className="col text-center" style={{ width: "100%" }}>
+          <img
+            src={`https://image.tmdb.org/t/p/original/${detail.backdrop_path}`}
+            alt={detail.title}
+            className="img-fluid"
+          />
+          <div className="carousel-center">
+            <i
+              onClick={() => setIsOpen(true)}
+              className="far fa-play-circle"
+              style={{ fontSize: 95, color: "#f4c10f", cursor: "pointer" }}
+            ></i>
+          </div>
+          <div
+            className="carousel-caption"
+            style={{ textAlign: "center", fontSize: 35 }}
+          >
+            {detail.title}
+          </div>
+        </div>
+      </div>
+      <div className="row mt-3">
+        <div className="col">
+          <p style={{ color: "#5a606b", fontWeight: "bolder" }}>GENRE</p>
+        </div>
+      </div>
+      <div className="row mt-3">
+        <div className="col">
+          <ul className="list-inline">{genresList}</ul>
+        </div>
+      </div>
+      <div className="row mt-3">
+        <div className="col">
+          <div className="text-center">
+            <ReactStars
+              count={detail.vote_average}
+              size={20}
+              color1={"#f4c10f"}
+            ></ReactStars>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
